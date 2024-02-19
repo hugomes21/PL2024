080b46d7fa75a5be5d1c25b1cc6563aae47019a4
hugomes21 <a96842@alunos.uminho.pt>
2024-02-19 14:35:33 +0000

    cricao de pastas

---
+++ TPC1/tpc1.py
@@ -0,0 +1,9 @@
+''' Tpc PL ————————— // —————-
+Tenho um csv
+Nao usar expressões regulares (?)
+Proibido usar o modulo csv
+Ler linha a linha e “partir” nos campos correspondentes (usar a funcao split)
+lista de modalidades ordenada alfabeticamente
+Percentagem de atletas aptos e inaptos
+Distribuição de atletas por escalão etario(…, [30-34], [35-39], [40-44], …)
+'''
